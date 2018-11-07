4c6384b92fc515fe8e60058c9ba4633f654d34e2
a lan <bluenowte97@gmail.com>
2018-11-07 03:35:57 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,14 @@
 def my_select(collection)
  # your code here!
-  # if collection.length == 0
-  #   "This block should not run!"
-  #   
+  if collection.length == 0
+    "This block should not run!"
+  
    new_collection = []
    i = 0
    while i < collection.length
      new_collection << yield(collection[i])
+     i+= 1
+     ## note must increase i because we are using while loop
    end
-   new_collection.compact
+   new_collection
 end
