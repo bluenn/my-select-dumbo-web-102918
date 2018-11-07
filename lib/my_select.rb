697463da20ecd28b76025e9cc061379db4024bd8
a lan <bluenowte97@gmail.com>
2018-11-07 03:05:20 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+ if collection.length == 0
+   return "This block should not run!"
+
+ else
+   new_collection[]
+   i = 0
+   while i < collection.length
+     new_collection << yield collection[i]
+   end
+   new_collection
 end
