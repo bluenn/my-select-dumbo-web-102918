cef2fe111755c6f559b1b34a7c6394891c1aa75d
a lan <bluenowte97@gmail.com>
2018-11-07 03:10:21 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -7,7 +7,7 @@ def my_select(collection)
    new_collection[]
    i = 0
    while i < collection.length
-     new_collection << yield (collection[i])
+     new_collection << yield(collection[i])
    end
    new_collection
  end
